c85adec0cc7a814f9a9fa2f2ab295f67ee331c29
Oleksandr <husiev95@gmail.com>
2022-01-03 00:10:28 -0600

    add shacl context shapes

---
+++ src/test/java/org/dbpedia/integration/SemanticValidationTests.java
@@ -0,0 +1,22 @@
+package org.dbpedia.integration;
+
+
+import lombok.extern.log4j.Log4j;
+import org.junit.Test;
+
+/**
+ * This test
+ */
+@Log4j
+public class SemanticValidationTests {
+
+    @Test
+    public void testContextExists(){
+
+    }
+
+    @Test
+    public void testLinksExist(){
+
+    }
+}
